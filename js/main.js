e2bdbc4f8c76806034ed14008ddbad1b252b21b0
Sergejs <sergejs@symplectic.co.uk>
2023-04-28 21:30:53 +0100

    try for video load

--- js/main.js
+++ js/main.js
@@ -442,3 +442,8 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-0M4GEK1QY7');
+
+// Load video 
+var video = document.getElementById('video');
+video.load();
+video.play();
