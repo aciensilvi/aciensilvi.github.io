5932a2cb90dcfe4156c0e0c39d8fa4330988e04a
Sergejs <sergejs@symplectic.co.uk>
2023-04-29 22:02:21 +0100

    animate logo on click

--- js/main.js
+++ js/main.js
@@ -471,3 +471,35 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'G-0M4GEK1QY7');
+
+// Animate homepage logo
+const animateCSS = (element, animation, prefix = 'animate__') =>
+// We create a Promise and return it
+new Promise((resolve, reject) => {
+  const animationName = `${prefix}${animation}`;
+  const node = document.querySelector(element);
+
+  node.classList.add(`${prefix}animated`, animationName);
+  //node.classList.add(`${prefix}animated`, animationName).setProperty('--animate-duration', '2s');
+
+  // When the animation ends, we clean the classes and resolve the Promise
+  function handleAnimationEnd(event) {
+    event.stopPropagation();
+    node.classList.remove(`${prefix}animated`, animationName);
+    resolve('Animation ended');
+  }
+
+  node.addEventListener('animationend', handleAnimationEnd, {once: true});
+});
+
+document.getElementById("homepageLogo").addEventListener("click", animateElement);
+
+function animateElement() {
+switch(Math.floor(Math.random() * 5)) {
+  case 0 : animateCSS('.homepageLogo', 'flip'); break;
+  case 1 : animateCSS('.homepageLogo', 'heartBeat'); break;
+  case 2 : animateCSS('.homepageLogo', 'hinge'); break;
+  case 3 : animateCSS('.homepageLogo', 'rubberBand'); break;
+  case 4 : animateCSS('.homepageLogo', 'tada'); break;
+}
+}
